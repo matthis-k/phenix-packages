f4c59bb6bebbc150e1c155dbc7d07d4dbafb34b6
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 14:47:48 +0200

    feat: add flakeModules.default with hello-<name> overlay

---
+++ modules/flake-module.nix
@@ -0,0 +1,9 @@
+{ ... }: {
+  phenix.overlays = [(final: prev: {
+    phenix = (prev.phenix or {}) // {
+      hello-packages = final.writeShellScriptBin "hello-packages" ''
+        echo "hello from packages"
+      '';
+    };
+  })];
+}
