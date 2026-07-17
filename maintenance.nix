253fda3b6caf5114b59e1c06c9b1968a5cb76d1e
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-17 11:12:45 +0200

    style: format maintenance module

--- maintenance.nix
+++ maintenance.nix
@@ -1,5 +1,4 @@
 { pkgs, ... }:
-
 let
   nixSources = "find . -type f -name '*.nix' -not -path './.git/*' -not -path './.devenv/*'";
 in
@@ -7,28 +6,38 @@ in
   scripts = {
     "maintenance-check-format" = {
       packages = [ pkgs.findutils pkgs.nixfmt ];
-      exec = ''${nixSources} -exec nixfmt --check {} +'';
+      exec = ''
+        ${nixSources} -exec nixfmt --check {} +
+      '';
     };
+
     "maintenance-check-statix" = {
       packages = [ pkgs.statix ];
       exec = "statix check --ignore '.git/**'";
     };
+
     "maintenance-check-deadnix" = {
       packages = [ pkgs.deadnix ];
       exec = "deadnix --fail --no-lambda-arg --no-lambda-pattern-names";
     };
+
     "maintenance-check-flake" = {
       packages = [ pkgs.git pkgs.nix ];
       exec = "nix flake check --print-build-logs --keep-going";
     };
+
     "maintenance-fix-format" = {
       packages = [ pkgs.findutils pkgs.nixfmt ];
-      exec = ''${nixSources} -exec nixfmt {} +'';
+      exec = ''
+        ${nixSources} -exec nixfmt {} +
+      '';
     };
+
     "maintenance-fix-statix" = {
       packages = [ pkgs.statix ];
       exec = "statix fix";
     };
+
     "maintenance-fix-deadnix" = {
       packages = [ pkgs.deadnix ];
       exec = "deadnix --edit --no-lambda-arg --no-lambda-pattern-names";
@@ -40,17 +49,29 @@ in
     "maintenance:statix".exec = "maintenance-check-statix";
     "maintenance:deadnix".exec = "maintenance-check-deadnix";
     "maintenance:flake".exec = "maintenance-check-flake";
+
     "maintenance:check" = {
       exec = "true";
-      after = [ "maintenance:format" "maintenance:statix" "maintenance:deadnix" "maintenance:flake" ];
+      after = [
+        "maintenance:format"
+        "maintenance:statix"
+        "maintenance:deadnix"
+        "maintenance:flake"
+      ];
       before = [ "devenv:enterTest" ];
     };
+
     "maintenance:fix:format".exec = "maintenance-fix-format";
     "maintenance:fix:statix".exec = "maintenance-fix-statix";
     "maintenance:fix:deadnix".exec = "maintenance-fix-deadnix";
+
     "maintenance:fix" = {
       exec = "true";
-      after = [ "maintenance:fix:format" "maintenance:fix:statix" "maintenance:fix:deadnix" ];
+      after = [
+        "maintenance:fix:format"
+        "maintenance:fix:statix"
+        "maintenance:fix:deadnix"
+      ];
     };
   };
 }
