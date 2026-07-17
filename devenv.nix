f493383e297a35fd9e10ee14b101ad4832d5a557
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-17 10:50:10 +0200

    refactor: discover local maintenance modules

---
+++ devenv.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+
+let
+  maintenanceModules = builtins.filter (
+    path: builtins.baseNameOf path == "maintenance.nix"
+  ) (lib.filesystem.listFilesRecursive ./.);
+in
+{
+  imports = maintenanceModules;
+  enterTest = "";
+}
